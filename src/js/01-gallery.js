61a3844867cc8ffe025984abffb1a8438aa7b6dc
Piron Evgeniy <piron.johny@gmail.com>
2021-11-09 14:49:25 +0000

    HW 7/1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,38 +1,39 @@
 import { galleryItems } from "./gallery-items.js";
-// import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
-
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryEl = document.querySelector(".gallery");
-const galleryLinkEl = galleryEl.querySelector(".gallery__link");
 
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `);
-
-// instance.show();
-
-function stopDefAction(evt) {
+function modalElemnt(evt) {
   evt.preventDefault();
+  basicLightbox
+    .create(
+      `
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+      `
+    )
+    .show();
+  if (basicLightbox){
+    console.log(evt.target);
+  }
 }
 
 function createHTMLMarkup(elem) {
   galleryEl.insertAdjacentHTML(
     "beforeend",
     `<div class="gallery__item">
-        <a class="gallery__link" href="${elem.original}">
-          <img
-            class="gallery__image"
-            src="${elem.preview}"
-            data-source="${elem.original}"
-            alt="${elem.description}"
-          />
-        </a>
-      </div> `
+      <a class="gallery__link" href="${elem.original}">
+        <img
+          class="gallery__image"
+          src="${elem.preview}"
+          data-source="${elem.original}"
+          alt="${elem.description}"
+        />
+      </a>
+    </div> `
   );
 }
+
+galleryEl.addEventListener("click", modalElemnt);
 
 const genGalleryItem = (galleryItems) => {
   galleryItems.map((elem) => createHTMLMarkup(elem));
